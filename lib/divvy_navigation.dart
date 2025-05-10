33cd0a765ad7fc54ecc3a8ec1db8e35eaac1aa97
Bess Hooper <bess.hooper@icloud.com>
2025-05-10 13:00:55 -0700

    Create house & join house screens done, need db connection

---
+++ lib/divvy_navigation.dart
@@ -0,0 +1,119 @@
+import 'package:divvy/models/divvy_theme.dart';
+import 'package:divvy/providers/divvy_provider.dart';
+import 'package:divvy/screens/notifications.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:divvy/screens/settings.dart';
+import 'package:divvy/screens/calendar.dart';
+import 'package:divvy/screens/chores.dart';
+import 'package:divvy/screens/dashboard.dart';
+import 'package:divvy/screens/house_screen.dart';
+
+/// Handles bottom navigation flow for the app
+class DivvyNavigation extends StatefulWidget {
+  const DivvyNavigation({super.key});
+
+  @override
+  State<DivvyNavigation> createState() => _DivvyNavigationState();
+}
+
+class _DivvyNavigationState extends State<DivvyNavigation> {
+  int _selectedIndex = 2;
+  static const List<Widget> _widgetOptions = <Widget>[
+    Calendar(),
+    Chores(),
+    Dashboard(),
+    House(),
+    Settings(),
+  ];
+  late final List<Widget> _titles;
+  late final String _houseName;
+
+  @override
+  void initState() {
+    super.initState();
+    _houseName = Provider.of<DivvyProvider>(context, listen: false).houseName;
+    _titles = <Widget>[
+      Text('Calendar', style: DivvyTheme.screenTitle),
+      Text('Chores', style: DivvyTheme.screenTitle),
+      Text('Divvy', style: DivvyTheme.screenTitle),
+      Text(_houseName, style: DivvyTheme.screenTitle),
+      Text('Settings', style: DivvyTheme.screenTitle),
+    ];
+  }
+
+  // sets the item tapped as the screen to be displayed
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: _titles[_selectedIndex],
+        centerTitle: true,
+        scrolledUnderElevation: 0,
+        backgroundColor: DivvyTheme.background,
+        actions: [
+          InkWell(
+            onTap: () => _openNotificationsPage(context),
+            child: Container(
+              height: 50,
+              width: 50,
+              padding: EdgeInsets.only(right: 20),
+              child: Icon(Icons.notifications),
+            ),
+          ),
+        ],
+      ),
+      body: Container(
+        color: DivvyTheme.background,
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: Theme(
+        // make sure there's no splash
+        data: ThemeData(
+          splashColor: Colors.transparent,
+          highlightColor: Colors.transparent,
+        ),
+        child: BottomNavigationBar(
+          backgroundColor: DivvyTheme.background,
+          type: BottomNavigationBarType.fixed,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today),
+              label: 'Calendar',
+            ),
+            BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Chores'),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.dashboard),
+              label: 'Dashboard',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.people_rounded),
+              label: 'House',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: DivvyTheme.mediumGreen,
+          unselectedItemColor: DivvyTheme.lightGrey,
+          onTap: _onItemTapped,
+        ),
+      ),
+    );
+  }
+
+  // Opens the notifications page
+  void _openNotificationsPage(BuildContext context) {
+    Navigator.of(
+      context,
+    ).push(MaterialPageRoute(builder: (context) => Notifications()));
+  }
+}
---
+++ lib/screens/join_house.dart
@@ -0,0 +1,186 @@
+import 'package:divvy/divvy_navigation.dart';
+import 'package:divvy/models/divvy_theme.dart';
+import 'package:divvy/screens/create_house.dart';
+import 'package:divvy/util/dialogs.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+
+/// Allows user to join a house using a unique six-digit code.
+class JoinHouse extends StatefulWidget {
+  const JoinHouse({super.key});
+
+  @override
+  State<JoinHouse> createState() => _JoinHouseState();
+}
+
+class _JoinHouseState extends State<JoinHouse> {
+  late final String _email;
+  late TextEditingController _codeController;
+  @override
+  void initState() {
+    super.initState();
+    _email = FirebaseAuth.instance.currentUser!.email ?? '';
+    _codeController = TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    final double spacing = width * 0.05;
+    return Scaffold(
+      backgroundColor: DivvyTheme.background,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Align(
+            alignment: Alignment.center,
+            child: SizedBox(
+              // Add extra spacing on the bottom to make it easier to
+              // enter text in fields
+              height: height,
+              width: width * 0.85,
+              child: Column(
+                // render text entry fields
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: spacing),
+                  _headerText(spacing),
+                  SizedBox(height: spacing),
+                  _houseCodeInput(spacing, width),
+                  SizedBox(height: spacing),
+                  horizontalOrLine(spacing),
+                  SizedBox(height: spacing),
+                  Center(
+                    child: InkWell(
+                      onTap: () => _createHouse(context),
+                      child: SizedBox(
+                        height: 50,
+                        child: Text(
+                          'Create a house!',
+                          style: DivvyTheme.bodyBlack.copyWith(
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  /// Displays divvy logo & introduction text
+  Widget _headerText(double spacing) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Center(child: Text('Divvy', style: DivvyTheme.screenTitle)),
+        SizedBox(height: spacing),
+        Text('Hi, $_email!', style: DivvyTheme.bodyBlack),
+        SizedBox(height: spacing / 4),
+        Text(
+          'It looks like you\'re not in a house yet.',
+          style: DivvyTheme.largeHeaderBlack,
+        ),
+        SizedBox(height: spacing),
+        Text(
+          'Enter your house\'s code below to join.',
+          style: DivvyTheme.bodyBlack,
+        ),
+      ],
+    );
+  }
+
+  /// Renders prompt & code input box, along with join button
+  Widget _houseCodeInput(double spacing, double width) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          height: 50,
+          decoration: DivvyTheme.textInput,
+          padding: EdgeInsets.symmetric(horizontal: 15),
+          child: TextField(
+            autocorrect: false,
+            controller: _codeController,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: 'Enter code....',
+            ),
+          ),
+        ),
+        SizedBox(height: spacing),
+        Center(
+          child: InkWell(
+            onTap: () => _joinHouse(context),
+            child: Container(
+              alignment: Alignment.center,
+              height: 50,
+              width: width / 4,
+              decoration: DivvyTheme.greenBox,
+              child: Text('Join', style: DivvyTheme.largeBoldMedWhite),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  /// Join a house and push the home screen
+  void _joinHouse(BuildContext context) {
+    // TODO: obviously, replace with db call
+    final inputValid = _codeController.text == 'code';
+    if (inputValid) {
+      // Update user's db with the code
+      print('Adding user to house ${_codeController.text}');
+      // Push user to home page
+      Navigator.of(context).pushReplacement(
+        PageTransition(
+          type: PageTransitionType.fade,
+          child: DivvyNavigation(),
+          duration: Duration(milliseconds: 100),
+        ),
+      );
+    } else {
+      showErrorMessage(context, 'Invalid Code', 'This house doesn\'t exist.');
+      return;
+    }
+  }
+
+  /// Open create house page
+  void _createHouse(BuildContext context) {
+    Navigator.of(context).pushReplacement(
+      PageTransition(
+        type: PageTransitionType.fade,
+        child: CreateHouse(),
+        duration: Duration(milliseconds: 100),
+      ),
+    );
+  }
+}
+
+/// Shows 'or' surrounded by a horizontal divider.
+Padding horizontalOrLine(double spacing) {
+  return Padding(
+    padding: EdgeInsets.symmetric(vertical: spacing),
+    child: Row(
+      children: [
+        Flexible(flex: 2, child: Container(height: 1, color: Colors.grey)),
+        Flexible(
+          flex: 1,
+          child: Container(
+            alignment: Alignment.center,
+            child: Text('or', style: DivvyTheme.bodyBlack),
+          ),
+        ),
+        Flexible(flex: 2, child: Container(height: 1, color: Colors.grey)),
+      ],
+    ),
+  );
+}
