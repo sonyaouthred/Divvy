2245de7f1f1a257fcc39911a687f61000f428d2b
sonyaouthred <sonyaouthred@live.com>
2025-05-23 12:21:17 -0700

    Fixed to account for local version date errors

--- test/dates_edge_case_functionality_test.dart
+++ test/dates_edge_case_functionality_test.dart
@@ -13,7 +13,7 @@ void main() {
           startDate: start,
         ),
       );
-      expect(dates.length, 182);
+      expect(dates.length, 181);
       expect(dates.first, start);
       expect(dates.last, start.add(const Duration(days: 180)));
     });
@@ -27,7 +27,7 @@ void main() {
           startDate: start,
         ),
       );
-      expect(dates.length, 182);
+      expect(dates.length, 181);
     });
   });
 
